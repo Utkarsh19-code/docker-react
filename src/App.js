490d899fa5ae3b4f7c814b09252ca99a7a7336aa
Utkarsh Shrivastava <utkarshsrvstv19@gmail.com>
2021-04-13 14:16:26 +0530

    changed feature text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi this is Utkarsh.
+          Hi this is Utkarsh from feature.
         </p>
         <a
           className="App-link"
